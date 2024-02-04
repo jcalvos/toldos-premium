e99f6961c9288010320e9cbc2c092a8f0291be07
ORKUZ-RED\jcalv <jcalvos@gmail.com>
2024-02-04 13:35:08 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'toldos.premium',
+  appName: 'toldos-premium',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
